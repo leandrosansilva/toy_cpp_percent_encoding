f5ce5927e55d100b9f06a6c0ba920e5d5397b4d4
Leandro Santiago <leandrosansilva@gmail.com>
2016-07-26 07:26:15 +0200

    Make the code slower and less legible remove outer conditionals... :-)

--- percentage_encoding.h
+++ percentage_encoding.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <tuple>
 #include <iostream>
+#include <functional>
+#include <array>
 
 enum decoder_state
 {
@@ -15,7 +17,7 @@ enum decoder_state
 
 constexpr auto is_printable(char c) -> bool
 {
-  return (c >= '0' && c <= '9') 
+  return (c >= '0' && c <= '9')
           || (c >= 'a' && c <= 'z')
           || (c >= 'A' && c <= 'Z');
 }
@@ -23,64 +25,63 @@ constexpr auto is_printable(char c) -> bool
 constexpr auto has_hexa_base(char c) -> bool
 {
   return (c >= 'a' && c <= 'f')
-          || (c >= 'A' && c <= 'F') 
+          || (c >= 'A' && c <= 'F')
           || (c >= '0' && c <= '9');
 }
 
+struct decoder_context
+{
+  decoder_state state;
+  std::string acc;
+  char hex_comp;
+};
+
+const auto decoder_state_handlers = std::array<std::function<decoder_context(const decoder_context&, char)>, 3> {
+  // S0
+  [](const decoder_context& context, char c) -> decoder_context
+  {
+    if (c == '+')
+      return {decoder_state::S0, context.acc + " ", '\0'};
+
+    if (is_printable(c))
+      return {decoder_state::S0, context.acc + c, '\0'};
+
+    if (c == '%')
+      return {decoder_state::S1, context.acc, '\0'};
+
+    throw std::runtime_error(std::string{"Invalid input"});
+  },
+
+  // S1
+  [](const decoder_context& context, char c) -> decoder_context
+  {
+    if (!has_hexa_base(c))
+      throw std::runtime_error("Invalid state on S1");
+
+    return {decoder_state::S2, context.acc, c};
+  },
+
+  // S2
+  [](const decoder_context& context, char c) -> decoder_context
+  {
+    if (!has_hexa_base(c))
+      throw std::runtime_error("Invalid state on S2");
+
+    const auto encoded_value = {context.hex_comp, c, '\0'};
+
+    const auto encoded_char = std::stoi(encoded_value, nullptr, 16);
+
+    return {decoder_state::S0, context.acc + static_cast<char>(encoded_char), '\0'};
+  }
+};
+
 auto percentage_decode(const std::string &input) -> std::string
 {
-  return std::get<1>(std::accumulate(
-    begin(input), 
-    end(input), 
-    std::make_tuple(decoder_state::S0, std::string{}, char{0}), 
-    [](const std::tuple<decoder_state, std::string, char> &context, char c) {
-      const auto current_state = std::get<0>(context);
-      const auto acc = std::get<1>(context);
-      const auto comp1 = std::get<2>(context);
-
-      //std::cout << std::endl
-      //          << "On S" << current_state 
-      //          << "  : acc: \"" << acc << "\""
-      //          << "    c: " << c 
-      //          << "    comp1: " << comp1 
-      //          << std::endl;
-
-      if (current_state == S0) {
-        if (c == '+') {
-          return std::make_tuple(S0, acc + " ", '\0');
-        }
-
-        if (is_printable(c)) {
-          return std::make_tuple(S0, acc + c, '\0');
-        }
-
-        if (c != '%') {
-          throw std::runtime_error("Invalid char");
-        }
-
-        return std::make_tuple(S1, acc, '\0');
-      }
-
-      if (current_state == S1) {
-        if (!has_hexa_base(c)) {
-          throw std::runtime_error("Invalid state on S1");
-        }
-
-        return std::make_tuple(S2, acc, c);
-      }
-
-      if (current_state == S2) {
-        if (!has_hexa_base(c)) {
-          throw std::runtime_error("Invalid state on S2");
-        }
-
-        const auto encoded_value = {comp1, c, '\0'};
-
-        const auto encoded_char = std::stoi(encoded_value, nullptr, 16);
-
-        return std::make_tuple(S0, acc + static_cast<char>(encoded_char), '\0');
-      }
-
-      throw std::runtime_error("Invalid state ever");
-    }));
+  return std::accumulate(
+    begin(input),
+    end(input),
+    decoder_context{decoder_state::S0, std::string{}, char{0}},
+    [&](const decoder_context &context, char c) -> decoder_context {
+      return decoder_state_handlers[static_cast<std::size_t>(context.state)](context, c);
+    }).acc;
 }
